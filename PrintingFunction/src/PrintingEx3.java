d1e0d549c0ead6c370c290c1f232b093e76d4218
ghanshyam kumar <133628810+ghanshyam9263@users.noreply.github.com>
2025-02-19 12:46:44 +0530

    PrintinFunction

---
+++ PrintingFunction/src/PrintingEx3.java
@@ -0,0 +1,12 @@
+
+public class PrintingEx3 {
+   public static void main(String[] args) {
+		double a= 25.456;
+		System.out.printf("%f\n",a);
+		System.out.printf("%f%n",a);
+		System.out.format("%f\n",a);
+		System.out.format("%f%n",a);
+		
+		 
+}
+}
---
+++ PrintingFunction/src/PrintingEx4.java
@@ -0,0 +1,14 @@
+
+public class PrintingEx4 {
+   public static void main(String[] args) {
+		double a=56.25645;
+		int b=55;
+		System.out.print(a+"\n");
+		System.out.print(b+"\n");
+		System.out.print(a+"%n");
+		System.out.println(a);
+		System.out.println(b);
+		
+		 
+}
+}
